5ee0dccce2717efee1ccf0072f5a3f98e04ce6c9
mcianci2020 <marcos.cianci@gmail.com>
2022-11-21 16:48:03 -0300

    update datasource

--- datasources.tf
+++ datasources.tf
@@ -20,42 +20,43 @@ data "aws_vpc" "vpc-lab-eks" {
 }
 
 
-data "aws_subnet_ids" "private_subnet_ids" {
-  vpc_id = data.aws_vpc.vpc-lab-eks.id
-
-  filter {
-    name    = "tag:kubernetes.io/cluster/${var.name}"
-    values  = ["shared"]
-  }
-
-  filter {
-    name    = "tag:kubernetes.io/role/internal-elb"
-    values  = [1]
-  }
-}
-
-data "aws_subnet_ids" "public_subnet_ids" {
-  vpc_id = data.aws_vpc.vpc-lab-eks.id
-
-  filter {
-    name    = "tag:kubernetes.io/cluster/${var.name}"
-    values  = ["shared"]
-  }
-
-  filter {
-    name    = "tag:kubernetes.io/role/elb"
-    values  = [1]
-  }
-}
-
-
-data "aws_subnet" "private_subnets" {
-  for_each = data.aws_subnet_ids.private_subnet_ids.ids
-  id       = each.value
-}
-
-
-data "aws_subnet" "public_subnets" {
-  for_each = data.aws_subnet_ids.public_subnet_ids.ids
-  id       = each.value
-}
+#data "aws_subnet_ids" "private_subnet_ids" {
+#  vpc_id = data.aws_vpc.vpc-lab-eks.id
+#
+#  filter {
+#    name    = "tag:kubernetes.io/cluster/${var.name}"
+#    values  = ["shared"]
+#  }
+#
+#  filter {
+#    name    = "tag:kubernetes.io/role/internal-elb"
+#    values  = [1]
+#  }
+#}
+#
+#data "aws_subnet_ids" "public_subnet_ids" {
+#  vpc_id = data.aws_vpc.vpc-lab-eks.id
+#
+#  filter {
+#    name    = "tag:kubernetes.io/cluster/${var.name}"
+#    values  = ["shared"]
+#  }
+#
+#  filter {
+#    name    = "tag:kubernetes.io/role/elb"
+#    values  = [1]
+#  }
+#}
+#
+#
+#data "aws_subnet" "private_subnets" {
+#  for_each = data.aws_subnet_ids.private_subnet_ids.ids
+#  id       = each.value
+#}
+#
+#
+#data "aws_subnet" "public_subnets" {
+#  for_each = data.aws_subnet_ids.public_subnet_ids.ids
+#  id       = each.value
+#}
+#
