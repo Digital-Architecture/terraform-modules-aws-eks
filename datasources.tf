9527e67ffc2ba77221b2ea360136c1011b712a9e
mcianci2020 <marcos.cianci@gmail.com>
2022-11-21 16:21:08 -0300

    update datasource ...

--- datasources.tf
+++ datasources.tf
@@ -21,7 +21,7 @@ data "aws_vpc" "vpc-lab-eks" {
 
 
 data "aws_subnet_ids" "private_subnet_ids" {
-  vpc_id = var.vpc_id
+  vpc_id = data.aws_vpc.vpc-lab-eks.id
 
   filter {
     name    = "tag:kubernetes.io/cluster/${var.name}"
@@ -35,7 +35,7 @@ data "aws_subnet_ids" "private_subnet_ids" {
 }
 
 data "aws_subnet_ids" "public_subnet_ids" {
-  vpc_id = var.vpc_id
+  vpc_id = data.aws_vpc.vpc-lab-eks.id
 
   filter {
     name    = "tag:kubernetes.io/cluster/${var.name}"
